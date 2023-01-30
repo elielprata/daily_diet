0ce9a129b385a901c408f698edca17ab308e407d
Eliel Martins <elieuprata@gmail.com>
2023-01-30 19:30:21 -0300

    add back button on this component

--- src/components/Percent/index.tsx
+++ src/components/Percent/index.tsx
@@ -1,8 +1,10 @@
+import { BackButton } from "@components/BackButton";
 import { Arrow, Container, PercentText, Subtitle } from "./styles";
 
 export function Percent() {
   return (
     <Container>
+      <BackButton />
       <Arrow />
       <PercentText>92,00%</PercentText>
       <Subtitle>das refeições dentro da dieta</Subtitle>
