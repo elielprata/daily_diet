017da54bf7d1937d40d0b510d270a8e004dc2594
Eliel Martins <elieuprata@gmail.com>
2023-01-30 19:31:37 -0300

    complete Statistics screen

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,5 +1,37 @@
-import { Container } from "./styles";
+import { Percent } from "@components/Percent";
+import { StatisticsInfo } from "@components/StatisticsInfo";
+
+import { Container, Content, DoubleInfo, Text } from "./styles";
 
 export function Statistics() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Percent />
+
+      <Content>
+        <Text>Estatísticas gerais</Text>
+
+        <StatisticsInfo
+          value={22}
+          subtitle="melhor sequência de pratos dentro da dieta"
+        />
+
+        <StatisticsInfo value={109} subtitle="refeições registradas" />
+
+        <DoubleInfo>
+          <StatisticsInfo
+            type="SUCCESS"
+            value={99}
+            subtitle="refeições dentro da dieta"
+          />
+
+          <StatisticsInfo
+            type="FAILURE"
+            value={22}
+            subtitle="refeições fora da dieta"
+          />
+        </DoubleInfo>
+      </Content>
+    </Container>
+  );
 }
--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,3 +1,23 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View``;
+
+export const Content = styled.View`
+  padding: 20px;
+  align-items: center;
+`;
+
+export const Text = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_100};
+  `}
+  margin-bottom: 20px;
+`;
+
+export const DoubleInfo = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
