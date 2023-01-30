602f75019c5b50c7f9c1cb8e7c4a6b878ff23e15
Eliel Martins <elieuprata@gmail.com>
2023-01-30 17:44:27 -0300

    create component button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,24 @@
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export const Container = styled(TouchableOpacity)`
+  padding: 10px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+  border-radius: 6px;
+
+  align-items: center;
+  justify-content: center;
+
+  min-height: 50px;
+  max-height: 50px;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.WHITE};
+  `}
+`;
