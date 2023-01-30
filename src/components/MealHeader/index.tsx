c87a6e16ebd0ca79cc0565ca2d78a672da7c54e2
Eliel Martins <elieuprata@gmail.com>
2023-01-30 17:45:21 -0300

    create component Meal Header of List

---
+++ src/components/MealHeader/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Date } from "./styles";
+
+export function MealHeader() {
+  return (
+    <Container>
+      <Date>12.01.22</Date>
+    </Container>
+  );
+}
---
+++ src/components/MealHeader/styles.ts
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View``;
+
+export const Date = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_100};
+  `}
+`;
